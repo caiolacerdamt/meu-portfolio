48589b95001cf61527ee5995d65e3cf22ac59584
caiolacerdamt <caiolacerdamt@gmail.com>
2023-06-28 18:04:08 -0300

    change image and refactoring js

--- PORTFÓLIO ATUAL/scripts.js
+++ PORTFÓLIO ATUAL/scripts.js
@@ -9,7 +9,7 @@ const scrollreveal = {
 const scrollLeft = {
   duration: 2000,
   origin: "left",
-  distance: "30%",
+  distance: "20%",
   easing: "ease",
   scale: 0.9,
 };
@@ -17,25 +17,29 @@ const scrollLeft = {
 const scrollRight = {
   duration: 2000,
   origin: "right",
-  distance: "30%",
+  distance: "20%",
   easing: "ease",
   scale: 0.9,
 };
 
-sr.reveal(".eu", scrollreveal);
-sr.reveal("#exemplo-do-projeto1", scrollreveal);
-sr.reveal("#sobre-o-projeto1", scrollreveal);
-sr.reveal("#exemplo-do-projeto2", scrollreveal);
-sr.reveal("#sobre-o-projeto2", scrollreveal);
-sr.reveal(".formulario", scrollLeft);
-sr.reveal(".box-contatos", scrollRight);
+const revealMap = {
+  ".eu": scrollreveal,
+  "#exemplo-do-projeto1": scrollreveal,
+  "#sobre-o-projeto1": scrollreveal,
+  "#exemplo-do-projeto2": scrollreveal,
+  "#sobre-o-projeto2": scrollreveal,
+};
+
+for (const selector in revealMap) {
+  sr.reveal(selector, revealMap[selector]);
+}
 
 const btn = document.querySelector("#ver-mais");
 const projeto3 = document.querySelector(".projeto3");
 const projeto4 = document.querySelector(".projeto4");
 
 btn.addEventListener("click", () => {
-  if (projeto3.style.display && projeto4.style.display === "flex") {
+  if (projeto3.style.display === "flex" && projeto4.style.display === "flex") {
     projeto3.style.display = "none";
     projeto4.style.display = "none";
     btn.innerText = "VER MAIS";
@@ -50,5 +54,7 @@ btn.addEventListener("click", () => {
     const projeto3Element = document.querySelector(".projeto3");
     projeto3Element.scrollIntoView({ behavior: "smooth" });
   }
-});
 
+  sr.reveal(".formulario", scrollLeft);
+  sr.reveal(".box-contatos", scrollRight);
+});
